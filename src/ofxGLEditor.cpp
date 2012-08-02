76ea4cb7798d54746e7384a0bd56ee371767ebf7
danomatika <danomatika@gmail.com>
2012-08-02 18:02:49 -0400

    updated readme; added license; added screenshot

--- src/ofxGLEditor.cpp
+++ src/ofxGLEditor.cpp
@@ -1,13 +1,22 @@
 /*
- *  ofxGLEditor.cpp
- *  LiveCoding
+ * Copyright (C) 2012 Akira Hayasaka & Dan Wilcox <danomatika@gmail.com> 
  *
- *  Created by Makira on 11/07/05.
- *  Copyright 2011 ・‥…―━━━―…‥・yesMAYBEno・‥…―━━━―…‥・. All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	Updated by Dan Wilcox <danomatika@gmail.com> 2012
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * See https://github.com/Akira-Hayasaka/ofxGLEditor for more info.
  */
-
 #include "ofxGLEditor.h"
 
 // for mapping special keys
