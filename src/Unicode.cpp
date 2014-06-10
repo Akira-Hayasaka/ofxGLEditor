4d626bea8dc5ef81ba395565220e9c63c447d362
danomatika <danomatika@gmail.com>
2014-06-09 20:20:55 -0400

    small tweaks to fix a few warnings

--- src/Unicode.cpp
+++ src/Unicode.cpp
@@ -31,40 +31,40 @@ string wstring_to_string(const wstring &input)
         else if(input[i] < 0x800)
         {
             output.push_back((byte)(MASK2BYTES | input[i] >> 6));
-            output.push_back((byte)(MASKBYTE | input[i] & MASKBITS));
+            output.push_back((byte)(MASKBYTE | (input[i] & MASKBITS)));
         }
         // 1110xxxx 10xxxxxx 10xxxxxx
         else if(input[i] < 0x10000)
         {
-            output.push_back((byte)(MASK3BYTES | input[i] >> 12));
-            output.push_back((byte)(MASKBYTE | input[i] >> 6 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] & MASKBITS));
+            output.push_back((byte)(MASK3BYTES | (input[i] >> 12)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 6 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] & MASKBITS)));
         }
         // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
         else if(input[i] < 0x200000)
         {
-            output.push_back((byte)(MASK4BYTES | input[i] >> 18));
-            output.push_back((byte)(MASKBYTE | input[i] >> 12 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] >> 6 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] & MASKBITS));
+            output.push_back((byte)(MASK4BYTES | (input[i] >> 18)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 12 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 6 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] & MASKBITS)));
         }
         // 111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
         else if(input[i] < 0x4000000)
         {
-            output.push_back((byte)(MASK5BYTES | input[i] >> 24));
-            output.push_back((byte)(MASKBYTE | input[i] >> 18 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] >> 12 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] >> 6 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] & MASKBITS));
+            output.push_back((byte)(MASK5BYTES | (input[i] >> 24)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 18 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 12 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 6 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] & MASKBITS)));
         }
         // 1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
         else if(input[i] < 0x8000000)
         {
-            output.push_back((byte)(MASK6BYTES | input[i] >> 30));
-            output.push_back((byte)(MASKBYTE | input[i] >> 18 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] >> 12 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] >> 6 & MASKBITS));
-            output.push_back((byte)(MASKBYTE | input[i] & MASKBITS));
+            output.push_back((byte)(MASK6BYTES | (input[i] >> 30)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 18 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 12 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] >> 6 & MASKBITS)));
+            output.push_back((byte)(MASKBYTE | (input[i] & MASKBITS)));
         }
     }
     return output;
